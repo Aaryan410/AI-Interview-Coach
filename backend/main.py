3c5b61f50eff458672bdb73a76d2756e1117a7d0
Aaryan Jadav <jadavaaryan280@gmail.com>
2026-07-14 19:19:13 +0530

    Implemented question loading and terminal interview prototype, and modified naming in json files to prevent future bugs

--- backend/main.py
+++ backend/main.py
@@ -4,10 +4,12 @@ import random
 
 
 # Printing UI
-text = pyfiglet.figlet_format("AI Interview Coach", font = "standard", width = 200)
+text = pyfiglet.figlet_format("Crack", font = "standard", width = 200)
+text1 = pyfiglet.figlet_format("AI Interview Coach", font = "standard", width = 100)
 
 print('==========================================================================================')
 print(text.rstrip())
+print(text1.rstrip())
 print('==========================================================================================')
 
 # Asking for roles and topics
@@ -29,8 +31,12 @@ random_question_obj = random.choice(questions_list)
 
 
 # Printing questions
+
+print('---------------------------------------')
 print()
 print(random_question_obj)
+print()
+print('---------------------------------------')
 print()
 print(random_question_obj["question"])
 print()
